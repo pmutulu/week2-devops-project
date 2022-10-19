7686c08f96e6805eb1326291bca2e992ee5cbd2f
pmutulu <pmlabs.ke@gmail.com>
2022-10-19 10:50:13 +0300

    Added the Day of Week Function

---
+++ dow.js
@@ -0,0 +1,13 @@
+// Javascript program to find day of a given date
+  
+function dayofweek(d, m, y)
+{
+    let t = [ 0, 3, 2, 5, 0, 3, 5, 1, 4, 6, 2, 4 ];
+    y -= (m < 3) ? 1 : 0;
+    return ( y + y/4 - y/100 + y/400 + t[m-1] + d) % 7;
+}
+ 
+// Driver Code
+ 
+    let day = dayofweek(31, 3, 1990);
+    console.log(Math.round(day));
