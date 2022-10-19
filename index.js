b9e43bcc36961a986e8b0e095c02071f0e64ae95
pmutulu <pmlabs.ke@gmail.com>
2022-10-19 13:26:59 +0300

    Amended the dayOfWeekFunction

--- index.js
+++ index.js
@@ -1,60 +1,64 @@
-const valueVerifier = (dob,mob,yob,gender) => {
-    if (dob == '' && mob == '' &&  yob =='' && gender=='') {
+/*
+const valueVerifier = (dob,mob,yob) => {
+    if (dob == '' && mob == '' &&  yob =='') {
         alert('Please provide an Day of Month, Month of birth, Year of Birth and/or Gender');
     }
-}
+}*/
 
-const passwordVerifier = (value) => {
+/*const passwordVerifier = (value) => {
     if (value.length < 4) {
         alert('The password should be greater than 4 characters');
     }
-}
+}*/
 
-const userOne = { 'email': 'email@email.com', 'password': 'password' };
+//const userOne = { 'dob': '26', 'mob': '3', 'yob':'1990' };
 
-const loginFunction = (email, password) => {
-    if (email == userOne.email && password == userOne.password) {
-        const loginMessage = 'You are now logged is as ' + email;
+/*const loginFunction = (dob, mob, yob,gender) => {
+    if (dob == userOne.dob && mob == userOne.mob && yob== userOne.yob && gender == userOne.gender) {
+        const loginMessage = 'You are now logged is as ' + dob;
         alert(loginMessage);
     } else {
         alert('Please review your email and/or your password');
     }
-}
+}*/
 
 // Javascript program to find day of a given date
   
-function dayofweek(d, m, y)
+ function dayOfWeek(dob, mob, yob)
 {
     let t = [ 0, 3, 2, 5, 0, 3, 5, 1, 4, 6, 2, 4 ];
-    y -= (m < 3) ? 1 : 0;
-    return ( y + y/4 - y/100 + y/400 + t[m-1] + d) % 7;
+    yob -= (mob < 3) ? 1 : 0;
+    return ( yob + yob/4 - yob/100 + yob/400 + t[mob-1] + dob) % 7;
 }
  
-// Driver Code
- 
-    let day = dayofweek(31, 3, 1990);
-    console.log(Math.round(day));
+
 
 
 
 const loginHandler = () => {
     // Retrieve the email and password elements
-    const emailInput = document.getElementById('email');
-    const passwordInput = document.getElementById('password');
+    const dobInput = document.getElementById('dob');
+    const mobInput = document.getElementById('mob');
+    const yobInput = document.getElementById('yob');
 
     // Retrieve the email and password values
-    const email = emailInput.value;
-    const password = passwordInput.value;
+    const dob = dobInput.value;
+    const mob = mobInput.value;
+    const yob = yobInput.value;
 
     // Verify that email and password are not empty
-    valueVerifier(email, password);
+   // valueVerifier(dob, mob, yob);
 
     // Verify that the password is greater than 4 characters
-    passwordVerifier(password);
+    //passwordVerifier(password);
 
     // Check against existing users in the system to verify if the user is registered and if password matches
-    loginFunction(email, password);
+    //dayOfWeek(dob, mob, yob);
+    // Driver Code
+    let day = dayOfWeek(dob, mob, yob);
+    alert(Math.round(day));
 }
+
 
 
 
