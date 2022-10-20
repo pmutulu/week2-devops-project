b678626cc6cb9a6ca48d7b888650d0ff5c1bd4e8
pmutulu <pmlabs.ke@gmail.com>
2022-10-20 16:42:05 +0300

    Added if statement fo numberOfDay

--- index.js
+++ index.js
@@ -8,21 +8,28 @@ var getDayOfWeek = () => {
     var birthday = birthdayInput.value;
     var gender = genderInput.value;
 
-   alert(birthday);
-   alert(gender); 
-   /*const dayOfWeek = new Date(birthday);
-   let day = dayOfWeek.getDate();
-   alert(day);*/
-   var dayOfWeek = new Date(birthday);
-   var numberOfDay = dayOfWeek.getDay();
-  // alert("Week Day: " + dayOfWeek.getDay());
-  alert(numberOfDay);
+    alert(birthday);
+    alert(gender);
+    /*const dayOfWeek = new Date(birthday);
+    let day = dayOfWeek.getDate();
+    alert(day);*/
+    var dayOfWeek = new Date(birthday);
+    var numberOfDay = dayOfWeek.getDay();
+    // alert("Week Day: " + dayOfWeek.getDay());
+    alert(numberOfDay);
+    if (numberOfDay == 0 && gender == 'Male') {
+        var akanName = 'Your Akan Name is Kwasi';
+        alert(akanName);
+    } else {
+        alert('Your name is note yer loaded');
+    }
+
 }
 
 
 
 
 
- 
- 
-  
+
+
+
