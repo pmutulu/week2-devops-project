775ddbea2ba27d5a33c9de71f39035079d822b96
pmutulu <pmlabs.ke@gmail.com>
2022-10-20 16:56:37 +0300

    Added Akan Names for Male

--- index.js
+++ index.js
@@ -18,11 +18,36 @@ var getDayOfWeek = () => {
     // alert("Week Day: " + dayOfWeek.getDay());
     alert(numberOfDay);
     if (numberOfDay == 0 && gender == 'Male') {
-        var akanName = 'Your Akan Name is Kwasi';
-        alert(akanName);
+        alert("Your were born on Sunday and your Akan Name is Kwasi");
+    } else if (numberOfDay == 1 && gender == 'Male') {
+        alert("Your were born on Monday and your Akan Name is Adwoa");
+    } else if (numberOfDay == 2 && gender == 'Male') {
+        alert("Your were born on Tuesday and your Akan Name is Abenaa");
+    } else if (numberOfDay == 3 && gender == 'Male') {
+        alert("Your were born on Wednesday and your Akan Name is Akua");
+    } else if (numberOfDay == 4 && gender == 'Male') {
+        alert("Your were born on Thursday and your Akan Name is Yaa");
+    } else if (numberOfDay == 5 && gender == 'Male') {
+        alert("Your were born on Friday and your Akan Name is Afua");
+    } else if (numberOfDay == 6 && gender == 'Male') {
+        alert("Your were born on Saturday and your Akan Name is  Ama");
+    } else if (numberOfDay == 1 && gender == 'Female') {
+        alert("Your were born on Monday and your Akan Name is Adwoa");
+    } else if (numberOfDay == 2 && gender == 'Female') {
+        alert("Your were born on Tuesday and your Akan Name is Abenaa");
+    } else if (numberOfDay == 3 && gender == 'Female') {
+        alert("Your were born on Wednesday and your Akan Name is Akua");
+    } else if (numberOfDay == 4 && gender == 'Female) {
+        alert("Your were born on Thursday and your Akan Name is Yaa");
+    } else if (numberOfDay == 5 && gender == 'Female') {
+        alert("Your were born on Friday and your Akan Name is Afua");
+    } else if (numberOfDay == 6 && gender == 'Female') {
+        alert("Your were born on Saturday and your Akan Name is  Ama");
     } else {
         alert('Your name is note yer loaded');
     }
+
+
 
 }
 
